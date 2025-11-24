9c847cbc1a999b0a950c0f339b2951696688b6fb
Roman Gonzalez <open-source@roman-gonzalez.info>
2025-11-23 20:20:29 -0800

    chore: add pre-commit hooks

--- nix/devenvs/default.nix
+++ nix/devenvs/default.nix
@@ -4,4 +4,14 @@
   packages = [
     pkgs.nodejs_24
   ];
+
+  git-hooks.hooks = {
+    deadnix = {
+      enable = true;
+      settings.edit = true;
+    };
+    nixfmt-rfc-style.enable = true;
+    nil.enable = true;
+  };
+
 }
